6f6bfb0c238697b36236a0ea10db135680ae016c
Yash Singh <119106771+Yash6012@users.noreply.github.com>
2023-06-22 18:51:55 +0530

    for loop tutorial

---
+++ forloop15.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main(void)
+{
+    printf("Program for tutorial of for loop\n");
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        printf("The value of i is %d\n", i + 1);
+    }
+    
+}
